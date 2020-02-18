6cc0cdf9b90e91cfefc685fad7d4b8d9f1de2e4f
guytonoriji <guyton.m.oriji.578@gmail.com>
2020-02-17 22:37:52 -0500

    Updates

---
+++ precache-manifest.8cb2fda7ebc2b1882c3f79e1df918588.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878b58ebd7bb3e6f98c2893bb9362c89",
+    "url": "/reducer-todo/index.html"
+  },
+  {
+    "revision": "f443af127813d451ebbf",
+    "url": "/reducer-todo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "00aa245f9453c654953a",
+    "url": "/reducer-todo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f443af127813d451ebbf",
+    "url": "/reducer-todo/static/js/2.576abfb8.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/reducer-todo/static/js/2.576abfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00aa245f9453c654953a",
+    "url": "/reducer-todo/static/js/main.3d19342c.chunk.js"
+  },
+  {
+    "revision": "1b0e164991c3ee928f84",
+    "url": "/reducer-todo/static/js/runtime-main.defb0f64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reducer-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
